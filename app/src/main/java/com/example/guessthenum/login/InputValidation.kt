2b71eb2ac9bc65541405661bc88760ae27b8abd8
jurekt <tomasz.jurek@snpgroup.com>
2020-05-12 21:54:23 +0200

    backup before moving to android x

--- app/src/main/java/com/example/guessthenum/login/InputValidation.kt
+++ app/src/main/java/com/example/guessthenum/login/InputValidation.kt
@@ -1,4 +1,30 @@
 package com.example.guessthenum.login
 
-class InputValidation {
+import android.content.Context
+import android.support.design.widget.TextInputEditText
+import android.support.design.widget.TextInputLayout
+
+class InputValidation (private val context: Context) {
+
+    /**
+     * method to check InputEditText filled .
+     *
+     * @param textInputEditText
+     * @param textInputLayout
+     * @param message
+     * @return
+     */
+    fun isInputEditTextFilled(textInputEditText: TextInputEditText, textInputLayout: TextInputLayout, message: String): Boolean {
+        val value = textInputEditText.text.toString().trim()
+        if (value.isEmpty()) {
+            textInputLayout.error = message
+            hideKeyboardFrom(textInputEditText)
+            return false
+        } else {
+            textInputLayout.isErrorEnabled = false
+        }
+
+        return true
+    }
+
 }
